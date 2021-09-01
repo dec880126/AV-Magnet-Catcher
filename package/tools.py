c960ee398f50a86b4450e77c23c453073639bf7a
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-09-01 20:16:43 +0800

    Update tools.py

--- package/tools.py
+++ package/tools.py
@@ -1,9 +1,9 @@
 import os
 
-def make_html(input_list, fileName, titleList, magnetList, article_Code_List):
+def make_html(pageINFOs: list, fileName: str):
     '''
-    Read the url in the input_list, and make the HTML file
-    type input_list: list
+    Read the url in the imgLinks, and make the HTML file
+    type imgLinks: list
     '''
     path = "./" + fileName
 
@@ -11,30 +11,40 @@ def make_html(input_list, fileName, titleList, magnetList, article_Code_List):
     f = open(path, 'w', encoding="utf-8")
 
     # HTML declaration
-    f.write(f"""<!doctype html>\n<html>\n\t<head>\n\t\t<title>Auto SHT Picture Viewer</title>\n\t</head>\n""")
+    f.write("""<!doctype html>\n<html>\n\t<head>\n\t\t<title>AVMC Picture Viewer</title>\n\t<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.css" rel="stylesheet" />
+    <script>
+        $(document).ready(function(){
+            $('.bxslider').bxSlider();
+        });
+    </script></head>\n""")
 
     # CSS declaration
     f.write("""\t<style>\n\t\tbody{\n\t\t\tbackground: #ebe9f9; /* Old browsers */\n\t\t\tbackground: -moz-linear-gradient(top, #ebe9f9 0%, #d8d0ef 50%, #cec7ec 51%, #c1bfea 100%); /* FF3.6-15 */\n\t\t\tbackground: -webkit-linear-gradient(top, #ebe9f9 0%,#d8d0ef 50%,#cec7ec 51%,#c1bfea 100%); /* Chrome10-25,Safari5.1-6 */\n\t\t\tbackground: linear-gradient(to bottom, #ebe9f9 0%,#d8d0ef 50%,#cec7ec 51%,#c1bfea 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */\n\t\t\tfilter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ebe9f9', endColorstr='#c1bfea',GradientType=0 ); /* IE6-9 */\n\t\t}\n\t</style>\n""")
 
     # HTML body writing
     f.write("""\t<body>\n\t\t<div style="text-align:center;">\n""")
-    pageNum = 0
-    for url in input_list:
-        title = titleList[pageNum]
-        article_Code = article_Code_List[pageNum]
-        article_URL = "https://www.sehuatang.org/thread-" + article_Code + "-1-1.html"
-        if url == "None":
-            continue
-        elif url == "end of page":
-            to_write = "\t\t\t\t<br>\n\t\t\t\t<h3>" + magnetList[pageNum] + "</h3>\n"
-            f.write(to_write)
-            f.write("\t\t\t<hr />\n")
-            pageNum += 1
-        elif url == "Head of Page":
-            f.write(f"""\t\t\t<h2><a href="{article_URL}"  target="_blank">{title}</a></h2>\n""")
-        else:
-            # 可擴充: loading="lazy"
-            f.write("\t\t\t\t<img src = " + str(url) + """ width=auto""" + """ height=auto class="center">\n""")
+
+    for page in  pageINFOs:
+        link = page.link
+        title = page.title
+        imgLinks = page.imgLinks
+        magnet = page.magnet
+
+        for imgLink in imgLinks:
+            if imgLink == "Head of Page":
+                # 標題
+                f.write(f"""\t\t\t<h2><a href="{link}"  target="_blank">{title}</a></h2>\n\t\t\t\t<ul class="bxslider">\n""")
+            elif imgLink == "end of page":
+                # 頁尾
+                to_write = "\t\t\t\t</ul>\t\t\t\t<br>\n\t\t\t\t<h3><a href = " + magnet + ">下載連結</a></h3>\n"
+                f.write(to_write)
+                f.write("\t\t\t<hr />\n")
+            else:
+                # 圖片
+                # 可擴充: loading="lazy"
+                f.write("\t\t\t\t<li><img src = " + imgLink + """ class="center" /></li>\n""") #  width=50%""" + """ height=50%
 
     f.write("""\t\t\t<a>Copyright © 2021.</a><a href = "https://github.com/dec880126" target="_blank">Cyuan</a><a>&nbsp&nbspAll rights reserved. </a>\n\t\t</div>\n\t</body>\n</html>""")
     f.close()
