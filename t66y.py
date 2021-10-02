8e5f5e5852acf73ffe24f0df8cc086614627734d
CyuanHuang <dec880126@gmail.com>
2021-10-03 01:05:07 +0800

    完成T66Y資訊擷取ˋ
    下次開始要補爬蟲代理IP
    我的IP被鎖拉幹

--- t66y.py
+++ t66y.py
@@ -1,198 +1,204 @@
-from bs4.element import Tag
 import requests
 import bs4
 import time
-from datetime import datetime
-from rich.progress import track
 import webbrowser
+import re
+from datetime import datetime
+import rich
+from rich.progress import track
 # import concurrent.futures
 # with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
 #     executor.map(getData.get_today_article, fourms, homeCodes, todays, pages)
 
 import package.config as config
-import package.tools as tools
+from package.tools import make_html as html_maker
 # from package.magnet import get_magnet
 
 
-
-class Fourm():
-    def __init__(self, fourmName, infoDict) -> None:
-        self.fourmName = fourmName
-        self.infoDict = infoDict
-
 class Article():
-    def __init__(self, link: str, title: str, imgLinks: list, magnet: str) -> None:
-        self.link = link
-        self.title = title
-        self.imgLinks = imgLinks
-        self.magnet = magnet
+    def __init__(self) -> None:
+        self.title = ""
+        self.number = ""   
+        self.link = ""
+        self.magnet = ""
+        self.imgLinks = []
+        self.tag = ""
+        self.VR = False
 
-def get_todayLists(fourmName_zh: str, fourmName_en: str, scrabDate: str) -> list:
-    fourmDict = {}
-    urlDict = {
-        "無碼": "http://t66y.com/thread0806.php?fid=2&search=2"
-    }
+class T66Y():
+    def __init__(self) -> None:
+        self.todayList = []
+        self.articleINFO = {}
+        self.headers = {}
 
-    fourmDict[fourmName_zh] = Fourm(fourmName_zh, config.load_config(mode='t66y'))
+    def get_cookie(self) -> None:
+        _session = requests.session()
+        _session.get(
+            "http://t66y.com/thread0806.php?fid=2&search=2", 
+            headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
+            }
+        )
+        cookie = _session.cookies.values()[0]
+        
+        self.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
+        self.headers["Accept"] = "text/html"
+        self.headers["Cookie"] = cookie
 
-    res = requests.get(urlDict[fourmName_zh], headers=t66y_headers)
-    res.encoding = "gbk"
-    res.content.decode('gbk').encode('utf-8')
+    def get_todayList(self, fourmType: str, today: str = datetime.now().strftime("%Y-%m-%d")) -> None:
+        """
+        無碼: http://t66y.com/thread0806.php?fid=2&search=2&page=1
+        有碼: http://t66y.com/thread0806.php?fid=15&search=2&page=1
+        """
+        today = "2021-10-02"
+        if fourmType == "無碼":
+            urlBase = "http://t66y.com/thread0806.php?fid=2&search=2"
+        elif fourmType == "有碼":
+            urlBase = "http://t66y.com/thread0806.php?fid=15&search=2"
 
-    with open("./res.html", "w", encoding="utf-8") as f:
-        f.write(res.text)
+        urlList = []
 
-    htmlSoup = bs4.BeautifulSoup(res.text, "html.parser")
+        for pageNum in range(1, 10):
+            res = requests.get(
+                url = f'{urlBase}&page={pageNum}',
+                headers = self.headers
+            )
+            res.encoding = "gbk"
+            res.content.decode('gbk').encode('utf-8')
 
-    tbody = htmlSoup.find("tbody", attrs={"id": "tbody"})
-    articles = tbody.find_all("tr", attrs={"class": "tr3 t_one tac"})
-    articleURLs = []
-    # topMarks = []
+            soup = bs4.BeautifulSoup(res.text, "html.parser")
+            tbody = soup.find(
+                "tbody",
+                attrs={
+                    "style": "table-layout:fixed;",
+                    "id": "tbody"
+                }
+            )
+            trs = tbody.find_all("tr", attrs = {"class": "tr3 t_one tac"})
 
-    # show info of exclude
-    print(f"[*]關鍵字排除 -> {fourmDict[fourmName_zh].infoDict}")
-    for article in articles:    
-        # <----- 檢查標籤是否為「新作」 start ----->
-        ifNew = article.find("span").get("class")
-        if ifNew[0] != "newworks":
-            continue
-        # <----- 檢查標籤是否為「新作」 end ----->
+            articleNums_of_current_page = len(trs)
 
-        # <----- 篩選本日文章 start ----->
-        releaseDate = article.find("div", attrs={"class": "f12"}).find("span").get("title")
-        # print(releaseDate)
-        if len(releaseDate) < 6:
-            releaseDate = article.find("div", attrs={"class": "f12"}).find("span").get_text()
+            temp = []
+            for tr in trs:
+                try:
+                    releaseDate = tr.find("span", attrs = {"class": "s3"}).get("title")
+                except AttributeError:
+                    try:
+                        releaseDate = tr.find("span", attrs = {"class": "s5"}).get("title")
+                    except AttributeError:
+                        div = tr.find("div", attrs = {"class": "f12"})
+                        releaseDate = div.find("span").get_text()
+                if today not in releaseDate:
+                    urlList.extend(temp)
+                    self.todayList = urlList
+                    print(f"總共抓了 {len(urlList)} 部本日文章")
+                    return
 
-        # !FIXME: 待觀察 t66y 網站動態 Top-Marks好像只會出現一陣子
-        # elif releaseDate.startswith("置顶主题"):
-        #     print(f"置顶主题: {article.find('a', attrs={'target': '_blank'}).get_text()}")
-        #     url = article.find("a", attrs={"target": "_blank"})
-        #     if getRidof_keyWord(url, fourmDict):
-        #         continue
+                title_link = tr.find("a", attrs={"target": "_blank"})
+                title = title_link.get_text()
+                link = "http://t66y.com/" + title_link.get("href")
 
-        #     url_base = "http://t66y.com/"
-        #     url = url_base + url.get('href')
-        #     articleURLs.append(f"{url}")
-        #     continue
+                temp.append(link)                             
 
-        else:
-            releaseDate = releaseDate[-10:]
+                if title[0] == "[":
+                    (resolution, dataSize) = title.split("]")[0].replace("[", "").split("/")
+                elif title[0] == "【":
+                    (resolution, dataSize) = title.split("】")[0].replace("【", "").split("/")
+
+                numberParser = re.compile(r"\w+-\d+")
+                number = numberParser.search(title).group()
+                title = title.split(number)[1]
+                if "】" in title:
+                    title = title.split("】")[-1]
+                elif "]" in title:
+                    title = title.split("]")[-1]
+                
+                if "VR" in title:
+                    VR = True
+                    title = title = title.split("】")[-1]
+                else:
+                    VR = False
+                # print(f"{number}\n\t{title}")
+
+                self.articleINFO[link] = Article()
+                self.articleINFO[link].title = title
+                self.articleINFO[link].number = number
+                self.articleINFO[link].link = link
+                if number[0] in [str(integer) for integer in range(10)]:
+                    print("新增tag -> 素人已排除")
+                    self.tag = "素人已排除"
+                if VR:
+                    print("已偵測到VR作品")
+                    self.VR = True
+                # if tr is trs[articleNums_of_current_page - 1]:
+                #     print(f"這是第 {pageNum} 頁的最後一篇\n\t以上共 {articleNums_of_current_page} 篇文章")
             
-        # TODO: 測試完要重新開啟 此為挑選本日文章功能
-        if releaseDate != scrabDate:
-            continue
-        # <----- 篩選本日文章 end ----->        
+            urlList.extend(temp)
 
+    def articleParser(self, url: str) -> None:
+        res = requests.get(url, headers = self.headers)
+        res.encoding = "gbk"
+        res.content.decode('gbk').encode('utf-8')
 
-        url = article.find("a", attrs={"target": "_blank"})
+        soup = bs4.BeautifulSoup(res.text, "html.parser")
 
-        # <----- 關鍵字 start -----> 
-        # 排除
-        # print(f"{getRidof_keyWord(url, fourmDict)=}")
-        # print(f"{len(fourmDict[fourmName_zh].infoDict.values())=}")
-        if getRidof_keyWord(url, fourmDict) and fourmDict[fourmName_zh].infoDict['exclude'][0] != "":
-            continue
-        # <----- 關鍵字 end ----->
+        mainBlock = soup.find("td", attrs={"valign": "top"})
 
-        url_base = "http://t66y.com/"
-        url = url_base + url.get('href')
-        articleURLs.append(f"{url}")
+        downloadPage_link = mainBlock.find("a", attrs={"style": "cursor:pointer;color:#008000;"}).get("href")        
 
-    return articleURLs
+        temp = []
+        temp.append("Head of Page")
 
-def article_parser_core(url: str):
-    """
-    type url: str
-    rtype title: str
-    rtype imgLinks: list
-    rtype magnet: str
-    """
-    temp = []
-    res = requests.get(url, headers = t66y_headers)
-    res.encoding = "gbk"
-    res.content.decode('gbk').encode('utf-8')
+        imgs = mainBlock.find_all("img")
+        for image in imgs:
+            imgLink = image.get("src")
+            if imgLink == None:
+                continue
+            temp.append(imgLink)
 
-    soup = bs4.BeautifulSoup(res.text, "html.parser")
+        try:
+            gallery = mainBlock.find("div", attrs={"class": "cl-gallery"})
+            picblocks = gallery.find_all("a")
+            for picBlock in picblocks:
+                imgLink = picBlock.get("href")
+                temp.append(imgLink)
+        except AttributeError:
+            print(f"AttributeError -> {url}")
+        temp.append("end of page")
+        imgLinks = temp.copy()
 
-    mainBlock = soup.find_all("td", attrs={"valign": "top"}, limit=1)[0]
+        self.articleINFO[url].magnet = downloadPage_link
+        self.articleINFO[url].imgLinks = imgLinks
 
-    title = mainBlock.find("h4").get_text()
-    downloadPage_link = mainBlock.find_all("a", attrs={"style": "cursor:pointer;color:#008000;"}, limit=1)[0].get_text()
+    def parse_article_in_todayList(self):
+        print("Parser Start")
+        for url in self.todayList:
+            print(f"正在分析: {url}")
+            self.articleParser(url)
 
-    imgLinks = mainBlock.find_all("img")
-    # _class_image_big = mainBlock.find_all("div", attrs={"class": "image-big"})
-    # imgLinks = mainBlock.find_all("img")
+    def make_html(self, fileName: str) -> None:
+        html_maker(self.articleINFO.values(), fileName)
 
-    temp.append("Head of Page")
-    # for block in _class_image_big:
-    #     try:
-    #         if block.get("src") == None:
-    #             t = block.get("ess-data")
-    #             temp.append(t)
-    #             continue
-    #         t = block.get("src")
-    #     except AttributeError:
-    #         pass
-    #     if "contents-thumbnail2.fc2.com/w240/" in t:
-    #         # 將fc2小圖換成大圖
-    #         t = t.replace("contents-thumbnail2.fc2.com/w240/", "")
-    #     if "s.jpg" in t:
-    #         t = t.replace("s.jpg", ".jpg")
-    #     temp.append(t)
-    for image in imgLinks:
-        img_link = image.get("ess-data")
-        if img_link == None:
-            continue
-        if "thumbnail" in img_link:
-            # img_link = img_link.replace("contents-thumbnail2.fc2.com/w240/", "")
-            continue
-        temp.append(img_link)
-    temp.append("end of page")
-    imgLinks = temp.copy()    
-    
-    articleDict[url] = Article(url, title, imgLinks, downloadPage_link)
+    def show(self):
+        print(f"{self.todayList=}")
+        # for article in self.articleINFO.values():
+        #     print(f"{article.title}")
+        #     print(f"{article.link}")
+        #     print(f"{article.magnet}")
+        #     print(f"{article.imgLinks}")
 
-def article_parser_executor(urls: list):
-    print("[*]正在分析文章資訊...")
-    start_time = time.time()
-    for progress, url in zip(track(urls, description='[>]正在分析文章'), urls):
-        article_parser_core(url)
-    end_time = time.time()
-    print(f"[*]一共花了 {end_time - start_time:2.2f} 秒來分析 {len(urls)} 篇文章")
+test = T66Y()
 
-def getRidof_keyWord(url: Tag, fourmDict: dict) -> bool:
-    """
-    fourmDict["無碼"].infoDict["exclude"]: 要排除之標題關鍵字
-    """
-    return any([filter in url.get_text() for filter in fourmDict["無碼"].infoDict["exclude"]])
+test.get_cookie()
 
-def start(scrabDate: str):
-    _session = requests.session()
-    _session.get(
-        "http://t66y.com/thread0806.php?fid=2&search=2", 
-        headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84"
-        }
-    )
-    Cookies = {
-        "t66y": _session.cookies.values()[0]
-    }
+test.get_todayList(fourmType = "有碼")
 
-    global t66y_headers
-    t66y_headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.84",
-        "Accept": "text/html",
-        "Cookie": Cookies["t66y"]
-    }
+test.parse_article_in_todayList()
 
-    links = get_todayLists(fourmName_zh="無碼", fourmName_en="Uncensored", scrabDate=scrabDate)
+# test.show()
 
-    global articleDict
-    articleDict = {}
-    article_parser_executor(links)
+test.make_html("測試.html")
 
-    fileName = "AVMC-Viewer-T66Y-" + "無碼" + ".html"
-    tools.make_html(articleDict.values(), fileName)
-    webbrowser.open_new_tab(fileName)
-
+# test.get_cookie()
+# test.articleParser("http://t66y.com/htm_data/2110/15/4719933.html")
+# test.make_html("./測試.html")
